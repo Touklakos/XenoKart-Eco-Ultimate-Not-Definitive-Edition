ee1ba8b7353c93257c31e4f2c0d1a36c5fa614c8
Riviere Benjamin <s172032@ic2s120-16.univ-lemans.fr>
2019-03-01 12:01:42 +0100

    maj menu

--- code/menu/menu.c
+++ code/menu/menu.c
@@ -5,7 +5,36 @@
 #include "menu.h"
 #include "../combat/const.h"
 
+const int tailleBouton = 100;
+const int decallage = 15;
+
+
+
+void affichage(SDL_Rect jeu, SDL_Rect option, SDL_Rect quitter, SDL_Rect choix, SDL_Surface* pSurface, SDL_Window* screen){
+
+  SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 0, 0, 0));
+
+  SDL_FillRect(pSurface, &jeu, SDL_MapRGB(pSurface->format, 255, 255, 255));
+  SDL_FillRect(pSurface, &option, SDL_MapRGB(pSurface->format, 255, 255, 255));
+  SDL_FillRect(pSurface, &quitter, SDL_MapRGB(pSurface->format, 255, 255, 255));
+
+  SDL_FillRect(pSurface, &choix, SDL_MapRGB(pSurface->format, 255, 0, 0));
+  SDL_UpdateWindowSurface(screen);
+}
+
 int main(int argc, char** argv){
+affichage(jeu, option, quitter, choix, pSurface);
+  SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
+  IMG_Init(IMG_INIT_PNG);
+
+  SDL_Window* screen = SDL_CreateWindow("Menu - XenoKart ECO PLUS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+
+  //TTF_Font *police = NULL;
+  //police = TTF_OpenFont("./data/DejaVuSans.ttf", 30);
+
+  SDL_Surface* pSurface = NULL;
+  pSurface = SDL_GetWindowSurface(screen);
 
   int quit = 0;
   int fin = 0;
@@ -13,49 +42,21 @@ int main(int argc, char** argv){
   int debut = 0;
   int fpsCount = 0;
 
-  int tailleBouton = 100;
-
-
-  int decallage = 15;
-  int decallageBouton = tailleBouton+decallage;
   int nbChoix = 0;
-
-  SDL_Event event;
-
-  SDL_Init(SDL_INIT_EVERYTHING);
-  TTF_Init();
-  IMG_Init(IMG_INIT_PNG);
-
-  SDL_Window* screen = SDL_CreateWindow("Menu - XenoKart ECO PLUS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
-
-  TTF_Font *police = NULL;
-  police = TTF_OpenFont("./data/DejaVuSans.ttf", 30);
-
-  SDL_Surface* pSurface = NULL;
-  pSurface = SDL_GetWindowSurface(screen);
+  int decallageBouton = tailleBouton+decallage;
 
   SDL_Rect jeu = {0,0*decallageBouton,tailleBouton,tailleBouton};
   SDL_Rect option = {0,1*decallageBouton,tailleBouton,tailleBouton};
-  SDL_Rect credit = {0,2*decallageBouton,tailleBouton,tailleBouton};
-  SDL_Rect quitter = {0,3*decallageBouton,tailleBouton,tailleBouton};
-
+  SDL_Rect quitter = {0,2*decallageBouton,tailleBouton,tailleBouton};
   SDL_Rect choix = {0,nbChoix*decallage,tailleBouton,tailleBouton};
 
+  SDL_Event event;
 
-  SDL_FillRect(pSurface, &jeu, SDL_MapRGB(pSurface->format, 255, 255, 255));
-  SDL_FillRect(pSurface, &option, SDL_MapRGB(pSurface->format, 255, 255, 255));
-  SDL_FillRect(pSurface, &credit, SDL_MapRGB(pSurface->format, 255, 255, 255));
-  SDL_FillRect(pSurface, &quitter, SDL_MapRGB(pSurface->format, 255, 255, 255));
-
-  //SDL_BlitSurface(TTF_RenderText_Solid(police, "Jeu", couleur), NULL, pSurface, &dest);
-  TTF_CloseFont(police);
+  //  SDL_BlitSurface(TTF_RenderText_Solid(police, "Jeu", couleur), NULL, pSurface, &jeu);
+  //  TTF_CloseFont(police);
 
 
-  SDL_FillRect(pSurface, &choix, SDL_MapRGB(pSurface->format, 255, 0, 0));
-
-
-
-  SDL_UpdateWindowSurface(screen);
+  affichage(jeu, option, quitter, choix, pSurface, screen);
 
   while(!quit){
 
@@ -71,19 +72,11 @@ int main(int argc, char** argv){
       if(state[SDL_SCANCODE_S]){
 
         nbChoix++;
-        if(nbChoix == 4) nbChoix = 0;
+        if(nbChoix == 3) nbChoix = 0;
 
         choix.y = nbChoix*decallageBouton;
 
-        SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 0, 0, 0));
-
-        SDL_FillRect(pSurface, &jeu, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &option, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &credit, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &quitter, SDL_MapRGB(pSurface->format, 255, 255, 255));
-
-        SDL_FillRect(pSurface, &choix, SDL_MapRGB(pSurface->format, 255, 0, 0));
-        SDL_UpdateWindowSurface(screen);
+        affichage(jeu, option, quitter, choix, pSurface, screen);
 
         recupCurs = DELAI_CURSEUR;
       }
@@ -91,20 +84,11 @@ int main(int argc, char** argv){
       if(state[SDL_SCANCODE_W]){
 
         nbChoix--;
-        if(nbChoix == -1) nbChoix = 3;
+        if(nbChoix == -1) nbChoix = 2;
 
         choix.y = nbChoix*decallageBouton;
 
-
-        SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 0, 0, 0));
-
-        SDL_FillRect(pSurface, &jeu, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &option, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &credit, SDL_MapRGB(pSurface->format, 255, 255, 255));
-        SDL_FillRect(pSurface, &quitter, SDL_MapRGB(pSurface->format, 255, 255, 255));
-
-        SDL_FillRect(pSurface, &choix, SDL_MapRGB(pSurface->format, 255, 0, 0));
-        SDL_UpdateWindowSurface(screen);
+        affichage(jeu, option, quitter, choix, pSurface, screen);
 
         recupCurs = DELAI_CURSEUR;
 
@@ -119,9 +103,9 @@ int main(int argc, char** argv){
 
     fin = SDL_GetTicks();
 
-    printf("\nfps = %lld\n", (fpsCount++)*1000/SDL_GetTicks());
-    printf("fpsCount = %lld\n", fpsCount);
-    printf("fin = %lld\n", fin);
+    printf("\nfps = %i\n", (fpsCount++)*1000/SDL_GetTicks());
+    printf("fpsCount = %i\n", fpsCount);
+    printf("fin = %i\n", fin);
     int delai = ((1000/FPS)-(fin-debut));
 
     if(delai > 0){
