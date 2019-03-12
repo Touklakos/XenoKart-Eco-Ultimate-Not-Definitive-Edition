5841dc9ea910e8ec8fe6967a6f6a1e4de3a175d7
Riviere Benjamin <s172032@ic2s114-10.univ-lemans.fr>
2019-03-12 16:34:09 +0100

    map

--- code/map/map.c
+++ code/map/map.c
@@ -5,27 +5,129 @@
 #include "map.h"
 #include "../combat/const.h"
 
+int fpsCount = 0;
+int fin = 0;
+int deb = 0;
+int quit = 0;
+
+SDL_Event event;
+
+const int NB_CASE=20;
+
 int caseCount = 0;
 
-case_t * creerCase(){
-  case_t * hexcase = NULL ;
-  hexcase = malloc(sizeof(*hexcase));
+/**
+  \file map.c
+  \brief XenoKart Eco Plus
+  \author Benjamin Riviere
+  \version 0.01
+  \date 12 mars 2019
+*/
 
+/**
+    \fn void fonctionQuitter()
+    \brief Cette fonction vérifie si l'utilisateur veut fermer le programme avec la croix
+*/
+
+void fonctionQuitter(){
+
+  SDL_PollEvent(&event);
+
+  switch(event.type){
+      case SDL_QUIT:
+        quit = 1;
+        break;
+  }
+}
+
+/**
+    \fn void fonctionFin()
+    \brief Cette fonction affiche les statistiques à la fin des boucles dans les fonctions
+*/
+
+void fonctionFin(){
+  fin = SDL_GetTicks();
+
+  printf("\nfps = %i\n", (fpsCount++)*1000/SDL_GetTicks());
+  printf("fin = %i\n", fin/1000);
+  int delai = ((1000/FPS)-(fin-deb));
+
+  if(delai > 0){
+    SDL_Delay(delai);
+  }
+}
+
+case_t creerCase(){
+  case_t hexcase;
+  hexcase.val = caseCount;
   return hexcase;
 }
 
+case_t creerCaseVoisin(case_t hexcase){
+  case_t t[6];
+  for(int i = 0; i < 6; i++){
+    while(caseCount < NB_CASE-1){
+      if(hexcase.voisin[i] != NULL){
+        t[i] = creerCase();
+        ajouter(t[i]);
+        t[i].val = 0;
+        hexcase.voisin[i] = &t[i];
+        caseCount++;
+      }
+    }
+  }
+  return hexcase;
+}
 
 
 map_t * creerMap(enum typemap type){
   map_t * map = NULL ;
   map = malloc(sizeof(*map));
 
+  map->c[N/2][M/2] = creerCase();
+  creerCaseVoisin(map->c[N/2][M/2]);
+
+
+
 
   return map;
 }
 
+void afficherMap(case_t c[N][M], SDL_Surface* pSurface, SDL_Window* screen){
+  SDL_Rect test = {(SCREEN_WIDTH/2)-(100)/2,(SCREEN_HEIGHT/2)-(100)/2,50,50};
+
+  SDL_Surface * img = NULL;
+  img = IMG_Load("code/map/test.png");
+  SDL_BlitSurface(img, NULL, pSurface, &test);
+
+  SDL_UpdateWindowSurface(screen);
+}
 
 int main(){
 
+  SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
+  IMG_Init(IMG_INIT_PNG);
 
+  SDL_Window* screen = SDL_CreateWindow("Menu - XenoKart ECO PLUS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+  SDL_Surface* pSurface = NULL;
+  pSurface = SDL_GetWindowSurface(screen);
+
+  map_t * map = creerMap(1);
+
+  while(!quit) {
+
+    deb = SDL_GetTicks();
+
+    SDL_PumpEvents();
+
+    afficherMap(map->c,pSurface, screen);
+    fonctionFin();
+    fonctionQuitter();
+  }
+  switch(quit){
+    case 1 : printf("Programme fermé par la croix\n"); break;
+    case 2 : printf("Programme fermé par le bouton\n"); break;
+  }
 }
--- code/map/map.h
+++ code/map/map.h
@@ -4,16 +4,41 @@
 
 #include "../combat/const.h"
 
+#define N 100
+#define M 50
+#define tmax 10
+
 enum typemap{ VOLCAN, JUNGLE, DESERT, TUNDRA, PLAINE, MARAIS, MONTAGNE, ARCHIPEL, PLATEAU };
 
 typedef struct case_s{
-  struct case_s *tab[6];
+  struct case_s *voisin[6];
   int val;
 }case_t;
 
 typedef struct map_s{
-  case_t * deb;
+  case_t c[N][M];
   enum typemap type;
   int difficult;
   int taille;
 }map_t;
+
+case_t file[tmax];
+int tete = 0, queue = tmax-1, nb=0;
+
+void ajouter(case_t c){
+  if(nb < tmax){
+    if(queue == tmax-1) queue = 0;
+    else queue++;
+    file[queue] = c;
+    nb++;
+  }
+}
+
+void retirer(case_t * c){
+  if(nb > 0){
+    *c = file[tete];
+    nb--;
+    if(tete == tmax-1) tete = 0;
+    else tete++;
+  }
+}
