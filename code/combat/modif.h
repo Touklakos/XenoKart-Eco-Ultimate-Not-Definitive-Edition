d0655dfc0c7251318eff1cbd6aa4aceaa0d44bdb
Touklakos <48084278+Touklakos@users.noreply.github.com>
2019-04-10 14:23:24 +0200

    Update modif.h

--- code/combat/modif.h
+++ code/combat/modif.h
@@ -1,5 +1,9 @@
 /*Structure qui permet de sauvegarder les valeurs de modifications des stats (valeur) pendant un certain temps (delai)*/
 
+/**
+    \struct Modif "Structure des modifications des statistiques"
+*/
+
 typedef struct {
 
     float valeur;
